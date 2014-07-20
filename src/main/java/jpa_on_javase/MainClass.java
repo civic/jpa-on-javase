f5c087d96c7a267cf038672e1818583df9a173f7
civic <tsskmail@gmail.com>
2014-07-20 22:09:29 +0900

    first commit
    H2databaseのEmbeddedモードでtarget/h2databaseを使用
    persistence.xmlで定義されたpersistence-unitを使用して接続

---
+++ src/main/java/jpa_on_javase/MainClass.java
@@ -0,0 +1,40 @@
+package jpa_on_javase;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+import jpa_on_javase.entity.Customer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * JavaSEからJPAを使うサンプル
+ */
+public class MainClass {
+    
+    public static void main(String[] args) throws SQLException {
+        //persistence.xmlで定義されたPersistenceUnitからEntityManagerFactoryを取得
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_on_javase_pu");
+        EntityManager em = emf.createEntityManager();
+
+        //Customerの追加
+        Customer customer = new Customer();
+        customer.setCustomerName("customer-" + System.currentTimeMillis());
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(customer);
+        tx.commit();
+
+        //Customerの検索
+        for (Customer c : em.createNamedQuery("Customer.findAll", Customer.class).getResultList()){
+            System.out.println(c);
+        }
+
+        em.close();
+        emf.close();
+
+    }
+}
---
+++ src/main/java/jpa_on_javase/entity/Customer.java
@@ -0,0 +1,83 @@
+package jpa_on_javase.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * EntityクラスCustomer。
+ * NetBeansのウィザードで自動生成
+ */
+@Entity
+@Table(name = "CUSTOMER")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c"),
+    @NamedQuery(name = "Customer.findByCustomerId", query = "SELECT c FROM Customer c WHERE c.customerId = :customerId"),
+    @NamedQuery(name = "Customer.findByCustomerName", query = "SELECT c FROM Customer c WHERE c.customerName = :customerName")})
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    @Basic(optional = false)
+    @Column(name = "CUSTOMER_ID")
+    private Integer customerId;
+    @Column(name = "CUSTOMER_NAME")
+    private String customerName;
+
+    public Customer() {
+    }
+
+    public Customer(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (customerId != null ? customerId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.customerId == null && other.customerId != null) || (this.customerId != null && !this.customerId.equals(other.customerId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer[ customerId=" + customerId + ", customerName='" + customerName + "' ]";
+    }
+    
+}
